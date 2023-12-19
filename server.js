aa455d4ad04c343b2b21a4cfc2c3f42aa6bff020
M4-dev <m4a.thenics@gmail.com>
2023-12-19 19:06:36 +0100

    renamed public to docs

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve static files (CSS, images, etc.) from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files (CSS, images, etc.) from the "docs" directory
+app.use(express.static(path.join(__dirname, 'docs')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
